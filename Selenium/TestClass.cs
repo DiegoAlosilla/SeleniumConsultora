9ec8de8fdd1ccb8a94eade50cbb8164b77aa8056
wilfredo morales <wmorales83@gmail.com>
2019-11-20 20:59:18 -0500

    Update

---
+++ Selenium/TestClass.cs
@@ -0,0 +1,30 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium.BaseClass;
+using System.Threading;
+
+namespace Selenium
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            //registrar Proyecto
+            driver.FindElement(By.XPath("//*[@id='navbarNav']/ul/li[4]/a")).Click();
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("/html/body/div/main/div/div/a")).Click();
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("/html/body/div/main/div[1]/div/form/div[1]/input")).SendKeys("Prueba con Selenium");
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("/html/body/div/main/div[1]/div/form/div[2]/input")).SendKeys("Prueba con Selenium");
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("/html/body/div/main/div[1]/div/form/div[3]/input")).Click();
+            Thread.Sleep(5000);
+        }
+    }
+}
