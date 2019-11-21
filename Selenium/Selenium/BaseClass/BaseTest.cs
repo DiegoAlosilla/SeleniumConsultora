5a8490e3ad378cd6e4f3bc2845aa758db8d267e3
DiegoAloslla <diegoalosilla@gmail.com>
2019-11-20 19:01:21 -0500

    create test Selenium

---
+++ Selenium/Selenium/BaseClass/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.BaseClass
+{
+    public class BaseTest
+    {
+
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver
+            {
+                Url = "http://localhost:9001/"
+            };
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
